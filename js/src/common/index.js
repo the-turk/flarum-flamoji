40c0201d659b730f72413ed2a1d6063427b26fb3
Darkle <tomasromero@outlook.com>
2023-02-07 20:38:07 +0100

    flarum-cli audit infra --fix

---
+++ js/src/common/index.js
@@ -0,0 +1,5 @@
+import app from 'flarum/common/app';
+
+app.initializers.add('the-turk/flarum-flamoji', () => {
+  console.log('[the-turk/flarum-flamoji] Hello, forum and admin!');
+});
--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,17 +1,1 @@
-const flarumConfig = require('flarum-webpack-config');
-const CopyPlugin = require('copy-webpack-plugin');
-
-let config = flarumConfig();
-
-config.plugins = config.plugins || [];
-
-config.plugins.push(new CopyPlugin([
-    
-        { 
-            from: "dist/vendors~*.js",
-            to: "../../assets"
-        }
-    
-]));
-
-module.exports = config;
+module.exports = require('flarum-webpack-config')();
